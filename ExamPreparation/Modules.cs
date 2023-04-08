7cc4b5d1c8e14dac5d6c002b8b1e6dfa6723f602
Дениска Зеленцов <zelentsov36@gmail.com>
2023-04-08 15:29:05 +0300

    fixed

--- ExamPreparation/Modules.cs
+++ ExamPreparation/Modules.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,96 @@ namespace ExamPreparation
             {
                 path = openFileDlg.FileName;
             }
+            return path;
+        }
+        public struct childrens
+        {
+            public string name { get; set; }
+            public string birthday { get; set; }
+            public string gender { get; set; }
+
+        }
+        public static List<childrens> ListOfChildrens = new List<childrens>();
+        public static List<childrens> ReadChildrens(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] fields = line.Split(',');
+                    try
+                    {
+                        ListOfChildrens.Add(new childrens { name = fields[0], birthday = fields[1], gender = fields[2] });
+                    }
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                }
+            }
+            return ListOfChildrens;
+        }
+        public static List<childrens> PremierGroup = new List<childrens>();
+        public static List<childrens> SecondGroup = new List<childrens>();
+        public static List<childrens> ThirdGroup = new List<childrens>();
+        public static List<childrens> FourthGroup = new List<childrens>();
+        public static List<childrens> Boys = new List<childrens>();
+        public static List<childrens> Girls = new List<childrens>();
+        public static void SortChildrens(List<childrens> Childrens)
+        {
+            try
+            {
+                foreach (var item in Childrens)
+                {
+                    if (GetAgeOfChildren(Convert.ToDateTime(item.birthday)) <= 3 && GetAgeOfChildren(Convert.ToDateTime(item.birthday)) >= 1)
+                    {
+                        PremierGroup.Add(item);
+                    }
+                    else if (GetAgeOfChildren(Convert.ToDateTime(item.birthday)) > 3 && GetAgeOfChildren(Convert.ToDateTime(item.birthday)) <= 7)
+                    {
+                        SecondGroup.Add(item);
+                    }
+                    else if (GetAgeOfChildren(Convert.ToDateTime(item.birthday)) > 7 && GetAgeOfChildren(Convert.ToDateTime(item.birthday)) <= 14)
+                    {
+                        ThirdGroup.Add(item);
+                    }
+                    else if (GetAgeOfChildren(Convert.ToDateTime(item.birthday)) > 14 && GetAgeOfChildren(Convert.ToDateTime(item.birthday)) <= 18)
+                    {
+                        FourthGroup.Add(item);
+                    }
+                }
+            }
+            catch (Exception)
+            { }
+            WriteChildrens(PremierGroup, GetPath("Premier.txt"));
+            WriteChildrens(SecondGroup, GetPath("Second.txt"));
+            WriteChildrens(ThirdGroup, GetPath("Third.txt"));
+            WriteChildrens(FourthGroup, GetPath("Fourth.txt"));
+        }
+        public static void WriteChildrens(List<childrens> Premier, string path)
+        {
+            
+            using (StreamWriter writer = new StreamWriter(File.Create(path)))
+            {
+
+                foreach (childrens item in Premier)
+                {
+                    string line = $"{item.name},{item.birthday},{item.gender}";
+                    writer.WriteLine(line);
+                }
+            }
+        }
+        public static int GetAgeOfChildren(DateTime birthday)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - birthday.Year;
+            return age;
+        }
+        public static string GetPath(string fileName)
+        { 
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
             return path;
         }
     }
--- ExamPreparation/Pages/MainPage.xaml.cs
+++ ExamPreparation/Pages/MainPage.xaml.cs
@@ -26,60 +26,18 @@ namespace ExamPreparation.Pages
         {
             InitializeComponent();
         }
-        List<string[]> PremierGroupWrite = new List<string[]>();
-        List<string[]> SecondGroupWrite = new List<string[]>();
-        List<string[]> ThirdGroupWrite = new List<string[]>();
-        List<string[]> FourthGroupWrite = new List<string[]>();
-        public struct childrens
-        { 
-            public string name { get; set; }
-            public string birthday { get; set; }
-            public string gender { get; set; }
-            public int age { get; set; }
 
-        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string path = Modules.OpenDialog();
-            List<childrens> PremierGroup = new List<childrens> ();
-            List<childrens> SecondGroup = new List<childrens>();
-            List<childrens> ThirdGroup = new List<childrens>();
-            List<childrens> FourthGroup = new List<childrens>();
-            using (StreamReader reader = new StreamReader(path))
-            {
-                DateTime now = DateTime.Today;
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    string[] fields = line.Split(',');
-                    DateTime birthday = Convert.ToDateTime(fields[1]);
-                    int age = now.Year - birthday.Year;
-                    if (age <= 3 && age >= 1)
-                    {
-                        PremierGroup.Add(new childrens() { name = fields[0], birthday = fields[1], gender = fields[2] });
-                        PremierGroupWrite.Add(fields);
-                    }
-                    else if (age > 3 && age <= 7)
-                    {
-                        SecondGroup.Add(new childrens() { name = fields[0], birthday = fields[1], gender = fields[2] });
-                        SecondGroupWrite.Add(fields);
-                    }
-                    else if (age > 7 && age <= 14)
-                    {
-                        ThirdGroup.Add(new childrens() { name = fields[0], birthday = fields[1], gender = fields[2] });
-                        ThirdGroupWrite.Add(fields);
-                    }
-                    else if (age > 14 && age <= 18)
-                    {
-                        FourthGroup.Add(new childrens() { name = fields[0], birthday = fields[1], gender = fields[2] });
-                        FourthGroupWrite.Add(fields);
-                    }
-                }
-            }
-            Premier.ItemsSource = PremierGroup;
-            Second.ItemsSource = SecondGroup;
-            Third.ItemsSource = ThirdGroup;
-            Fourth.ItemsSource = FourthGroupWrite;
+            List<Modules.childrens> Childrens = Modules.ReadChildrens(path);
+            Modules.SortChildrens(Childrens);
+            Childrens.Clear();
+            Premier.ItemsSource = Modules.PremierGroup;
+            Second.ItemsSource = Modules.SecondGroup;
+            Third.ItemsSource = Modules.ThirdGroup;
+            Fourth.ItemsSource = Modules.FourthGroup;
+
         }
 
         private void SavePremierClick(object sender, RoutedEventArgs e)
